51dd54be9274eb0a03fb1c23baac57e58948a7ba
unknown <farinangogta@gmail.com>
2024-12-23 11:50:13 -0500

    Actualizado 23/12/2024

--- Sistema_titulacion/tienda/urls.py
+++ Sistema_titulacion/tienda/urls.py
@@ -1,7 +1,17 @@
 from django.urls import path
 
-from .views import (crear_empresa)
+from .views import crear_empresa, nuevo_cliente, nueva_factura, nueva_linea_factura, nuevo_producto, nueva_categoria, nueva_marca, nueva_empresa, nuevo_proveedor
 
 urlpatterns = [
     path('dashboard/crear/', crear_empresa, name="crear_empresa"),
+
+    path('nuevo_cliente', nuevo_cliente, name="nuevo_cliente"),
+    path('nueva_factura', nueva_factura, name="nueva_factura"),
+    path('linea_factura', nueva_linea_factura, name="linea_factura"),
+    path('nuevo_producto', nuevo_producto, name="nuevo_producto"),
+    path('nueva_categoria', nueva_categoria, name="nueva_categoria"),
+    path('nueva_marca', nueva_marca, name="nueva_marca"),
+    path('nueva_empresa', nueva_empresa, name="nueva_empresa"),
+    path('nuevo_proveedor', nuevo_proveedor, name="nuevo_proveedor")
+
 ]
--- Sistema_titulacion/tienda/views.py
+++ Sistema_titulacion/tienda/views.py
@@ -27,3 +27,28 @@ def crear_empresa(request):
     }        
     
     return render(request, 'base.html', contexto)     
+
+
+def nuevo_cliente(request):
+    return render(request, 'nuevo_cliente.html')
+
+def nueva_factura(request):
+    return render(request, 'nueva factura.html')
+
+def nueva_linea_factura (request):
+    return render(request, 'linea_factura.html')
+
+def nuevo_producto(request):
+    return render(request, 'nuevo_producto.html')
+
+def nueva_categoria(request):
+    return render(request, 'nueva_empresa.html')
+
+def nueva_marca(request):
+    return render(request, "nueva_marca.html")
+
+def nuevo_proveedor(request):
+    return render(request, 'nuevo_proveedor.html')
+
+def nueva_empresa(request):
+    return render(request, 'nueva_empresa.html')
